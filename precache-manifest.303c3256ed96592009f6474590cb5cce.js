722ef0050c7f3ab25a110aace4d2ceb5460f63ba
michaelblack117 <michaelblack117@gmail.com>
2020-02-17 23:44:04 -0700

    Updates

---
+++ precache-manifest.303c3256ed96592009f6474590cb5cce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3df9607028207c828e687e10e68f56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcd96b60e254b00c41dd",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7e446a08460e11ec2b75",
+    "url": "/static/js/2.bae76b39.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.bae76b39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd96b60e254b00c41dd",
+    "url": "/static/js/main.caa47489.chunk.js"
+  },
+  {
+    "revision": "1cbdf4b3f955ed5ebcae",
+    "url": "/static/js/runtime-main.cc883f52.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
